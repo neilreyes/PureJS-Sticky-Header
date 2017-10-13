426ccccbedf919c0c7f189bc45eb0fe1d4a8eae8
Rayneil Reyes <rayneilreyes@gmail.com>
2017-10-13 18:20:37 +0800

    Initial commit sticky header

---
+++ script.js
@@ -0,0 +1,65 @@
+var onScrollAnimateHeader = function(){
+	var didScroll = false;
+	var lastScrollAmount = 0;
+	// Get #main-header height
+	var headerHeight = document.querySelector('header').offsetHeight + 50;
+	var bodyEl = document.body;
+	var className = 'has-scrolled';
+	var dContainer = document.getElementById('debug');
+
+	// Init debug
+	dContainer.innerHTML = 'Scroll Y: 0';
+
+	var init = function(){
+		// Triggers a scroll event function scrollPage after 1s/1000 ms
+		window.addEventListener('scroll',function(event){
+			// Check if didScroll is not true
+			if( !didScroll ){
+				// Set didScroll to true
+				didScroll = true;
+				setTimeout( scrollPage, 1000);
+			}
+		}, false);
+	}
+
+	function scrollPage(){
+		// Pass in the amount of window scroll Y axis
+		var pageScrollY = scrollY();
+		// Write Y axis
+		dContainer.innerHTML = 'Scroll Y: ' + pageScrollY;
+
+		// When scrolled down and are below the header, add a class .nav-up & .has-scrolled thus remove class .nav-dowm
+		if( pageScrollY > lastScrollAmount && pageScrollY > headerHeight ){
+			bodyEl.classList.remove('nav-down');
+			bodyEl.classList.add('nav-up','has-scrolled');
+		} else {
+			// When scrolled up show nav menu
+			if( pageScrollY + window.pageYOffset < document.documentElement.offsetHeight ){
+				bodyEl.classList.remove('nav-up');
+				bodyEl.classList.add('nav-down');
+
+				// When page scroll is at the top remove has-scrolled class
+				if( pageScrollY == 0 ){
+					bodyEl.classList.remove('has-scrolled');
+				}
+			}
+		}
+
+		// Set didScroll to false & record page scroll to last scroll
+		didScroll = false;
+		lastScrollAmount = pageScrollY;
+	}
+
+	// Gets the scroll amount of the window
+	function scrollY(){
+		return window.pageYOffset || document.documentElement.scrollTop;
+	}
+
+	obj = {
+		init: init,
+	}
+
+	return obj.init();
+}
+
+onScrollAnimateHeader();
